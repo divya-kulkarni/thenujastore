195e3e993398c64dfde42d8c77ad8b47c226c8cc
Divya Kulkarni <divyak98@gmail.com>
2024-02-11 20:06:26 +0900

    Created welcome page with timeout #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Welcome } from "./components/welcome";
+import { LandingPage } from "./components/landingPage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { NavBar } from "./components/navbar";
 
 function App() {
+  const [showWelcome, setShowWelcome] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowWelcome(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showWelcome ? (
+        <Welcome />
+      ) : (
+        <>
+          <LandingPage />
+        </>
+      )}
     </div>
   );
 }
---
+++ src/components/welcome.tsx
@@ -0,0 +1,6 @@
+import logo from "../assets/logo-white1.png";
+import "../styling/welcome.css";
+
+export const Welcome = () => {
+  return <div className="welcome"></div>;
+};
