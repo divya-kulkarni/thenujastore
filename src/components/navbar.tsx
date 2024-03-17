b6f5b89d386c0f2ebac2767355dfe79dd3c2f5e8
Divya Kulkarni <divyak98@gmail.com>
2024-03-17 21:34:43 +0900

    Fixed navbar dropdown issue in expanded state [#11]

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,3 @@
-import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Offcanvas from "react-bootstrap/Offcanvas";
@@ -13,97 +12,91 @@ export const NavBar = () => {
 
   return (
     <Navbar expand="md" id="navbar" fixed="top">
-      <Container className="navbar-container">
-        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md`} />
-        <Navbar.Brand onClick={() => navigate("/")}>
-          <img src={logo} alt="thenujastore" className="logo" />
-        </Navbar.Brand>
-        <Navbar.Offcanvas
-          id={`offcanvasNavbar-expand-md`}
-          aria-labelledby={`offcanvasNavbarLabel-expand-md`}
-          placement="start"
-        >
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-md`}>
-              thenujastore
-            </Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            <Nav className="justify-content-start flex-grow-1 pe-3 top-nav">
-              <Nav.Link className="main-link" onClick={() => navigate("/")}>
-                HOME
-              </Nav.Link>
-              <hr />
-              <NavDropdown title="SHOP BY CATEGORY" className="main-link">
-                <NavDropdown.Item
-                  href=""
-                  className="inner-link"
-                  onClick={() => navigate("/necklace")}
-                >
-                  NECKLACES
-                </NavDropdown.Item>
-                <NavDropdown.Item
-                  href=""
-                  className="inner-link"
-                  onClick={() => navigate("/bracelet")}
-                >
-                  BRACELETS
-                </NavDropdown.Item>
-                <NavDropdown.Item
-                  href=""
-                  className="inner-link"
-                  onClick={() => navigate("/earrings")}
-                >
-                  EARRINGS
-                </NavDropdown.Item>
-                <NavDropdown.Item
-                  href=""
-                  className="inner-link"
-                  onClick={() => navigate("/rings")}
-                >
-                  RINGS
-                </NavDropdown.Item>
-              </NavDropdown>
-              <hr />
-              <NavDropdown title="COLLECTIONS" className="main-link">
-                <NavDropdown.Item
-                  href=""
-                  className="inner-link"
-                  onClick={() => navigate("/necklace")}
-                >
-                  ROSEBUD
-                </NavDropdown.Item>
-                <NavDropdown.Item
-                  href=""
-                  className="inner-link"
-                  onClick={() => navigate("/bracelet")}
-                >
-                  FAIRYTALE
-                </NavDropdown.Item>
-                <NavDropdown.Item
-                  href=""
-                  className="inner-link"
-                  onClick={() => navigate("/earrings")}
-                >
-                  PASTEL DREAMS
-                </NavDropdown.Item>
-              </NavDropdown>
-              <hr />
-              <Nav.Link className="inner-link">FAQ</Nav.Link>
-              <hr />
-              <Nav.Link className="inner-link">Privacy Policy</Nav.Link>
-              <hr />
-            </Nav>
-          </Offcanvas.Body>
-        </Navbar.Offcanvas>
-        <NavbarText className="cart">
-          <img
-            src={shoppingCart}
-            alt="shopping cart"
-            onClick={() => navigate("/cart")}
-          />
-        </NavbarText>
-      </Container>
+      <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md`} />
+      <Navbar.Brand onClick={() => navigate("/")}>
+        <img src={logo} alt="thenujastore" className="logo" />
+      </Navbar.Brand>
+      <Navbar.Offcanvas
+        id={`offcanvasNavbar-expand-md`}
+        aria-labelledby={`offcanvasNavbarLabel-expand-md`}
+        placement="start"
+      >
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title id={`offcanvasNavbarLabel-expand-md`}>
+            thenujastore
+          </Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <Nav className="justify-content-start flex-grow-1 pe-3 top-nav">
+            <Nav.Link className="main-link" onClick={() => navigate("/")}>
+              HOME
+            </Nav.Link>
+            <hr />
+            <NavDropdown title="SHOP BY CATEGORY" className="main-link">
+              <NavDropdown.Item
+                href=""
+                className="inner-link"
+                onClick={() => navigate("/necklaces")}
+              >
+                NECKLACES
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                href=""
+                className="inner-link"
+                onClick={() => navigate("/bracelets")}
+              >
+                BRACELETS
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                href=""
+                className="inner-link"
+                onClick={() => navigate("/earrings")}
+              >
+                EARRINGS
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                href=""
+                className="inner-link"
+                onClick={() => navigate("/rings")}
+              >
+                RINGS
+              </NavDropdown.Item>
+            </NavDropdown>
+            <hr />
+            <NavDropdown title="COLLECTIONS" className="main-link">
+              <NavDropdown.Item
+                href=""
+                className="inner-link"
+                onClick={() => navigate("/necklace")}
+              >
+                ROSEBUD
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                href=""
+                className="inner-link"
+                onClick={() => navigate("/bracelet")}
+              >
+                FAIRYTALE
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                href=""
+                className="inner-link"
+                onClick={() => navigate("/earrings")}
+              >
+                PASTEL DREAMS
+              </NavDropdown.Item>
+            </NavDropdown>
+            <hr />
+          </Nav>
+        </Offcanvas.Body>
+      </Navbar.Offcanvas>
+      <NavbarText className="cart">
+        <img
+          src={shoppingCart}
+          alt="shopping cart"
+          onClick={() => navigate("/cart")}
+        />
+      </NavbarText>
     </Navbar>
   );
 };
